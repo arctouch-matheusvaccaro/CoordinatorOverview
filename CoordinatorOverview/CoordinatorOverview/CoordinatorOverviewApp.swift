b3586bc34973116f9ada398f8853042a91230426
Matheus Vaccaro <matheus.vaccaro@arctouch.com>
2024-08-16 12:38:29 -0300

    Add initial project

---
+++ CoordinatorOverview/CoordinatorOverview/CoordinatorOverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorOverviewApp.swift
+//  CoordinatorOverview
+//
+//  Created by Matheus Vaccaro on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorOverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
